f9d890db6bc1bfd743cb8f4427daf18e76de6445
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-04-22 18:09:17 +0430

    Add sms implementation

---
+++ Android/Sharing.Sms.cs
@@ -0,0 +1,18 @@
+﻿using Android.Content;
+using Android.Net;
+
+namespace Zebble.Device
+{
+    public partial class Sharing
+    {
+        public partial class Sms
+        {
+            public static void Share(string text)
+            {
+                var messageIntent = new Intent(Intent.ActionView, Uri.Parse("sms:" + ""));
+                messageIntent.PutExtra("sms_body", text);
+                UIRuntime.CurrentActivity.StartActivity(messageIntent);
+            }
+        }
+    }
+}
---
+++ UWP/Sharing.Sms.cs
@@ -0,0 +1,16 @@
+﻿using Windows.ApplicationModel.Chat;
+
+namespace Zebble.Device
+{
+    public partial class Sharing
+    {
+        public partial class Sms
+        {
+            public static void Share(string text)
+            {
+                var chat = new ChatMessage { Body = text };
+                ChatMessageManager.ShowComposeSmsMessageAsync(chat);
+            }
+        }
+    }
+}

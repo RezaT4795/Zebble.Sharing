e2a2d994e42f21b1a25e7a2ff7de83aea7313986
Ali Yousefi <ali.visual.studio@gmail.com>
2022-06-16 14:51:58 +0100

    add support for instagram (#1)
    * add support for instagram
    
    * move share file to base

---
+++ Android/Sharing.Instagram.cs
@@ -0,0 +1,19 @@
+﻿
+using Android.Content;
+using Android.Net;
+using AndroidX.Core.Content;
+using Java.IO;
+
+namespace Zebble.Device
+{
+    public partial class Sharing
+    {
+        public partial class Instagram
+        {
+            public static void ShareFile(string url, string title)
+            {
+                ShareFileToPackage("com.instagram.android", url, title);
+            }
+        }
+    }
+}
--- Android/Sharing.cs
+++ Android/Sharing.cs
@@ -2,11 +2,13 @@
 {
     using Android.App;
     using Android.Content;
-    using Context = Android.Content.Context;
+    using AndroidX.Core.Content;
+    using Java.IO;
+    using Olive;
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
-    using Olive;
+    using Context = Android.Content.Context;
 
     public partial class Sharing
     {
@@ -37,6 +39,21 @@
             clipboard.PrimaryClip = ClipData.NewPlainText(label ?? string.Empty, text);
 
             return Task.CompletedTask;
+        }
+
+        static void ShareFileToPackage(string package, string url, string title)
+        {
+            var shareIntent = new Intent(Intent.ActionSend);
+            shareIntent.SetType("image/*");
+            shareIntent.PutExtra(Intent.ExtraTitle, title);
+
+            File myFiles = new File(url);
+            Android.Net.Uri doneUri = FileProvider.GetUriForFile(UIRuntime.CurrentActivity.ApplicationContext,
+                    UIRuntime.CurrentActivity.PackageName + ".fileprovider", myFiles);
+
+            shareIntent.PutExtra(Intent.ExtraStream, doneUri);
+            shareIntent.SetPackage(package);
+            UIRuntime.CurrentActivity.StartActivity(shareIntent);
         }
     }
 }

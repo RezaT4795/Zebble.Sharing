6428489c4eb44251bf5fd1ec3bd75a1e188953d8
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-06-16 18:48:18 +0430

    Release

--- Android/Sharing.Twitter.cs
+++ Android/Sharing.Twitter.cs
@@ -9,15 +9,11 @@ namespace Zebble.Device
         public partial class Twitter
         {
             public static void Tweet(string text)
-            {
-                TweetLink(text, string.Empty);
-            }
+                => TweetLink(text, string.Empty);
 
             //TODO implement Twitter sharing photo
             public static void TweetPhoto(string text, byte[] photo)
-            {
-                throw new NotImplementedException();
-            }
+                => throw new NotImplementedException();
 
             public static void TweetLink(string text, string url)
             {
--- iOS/Sharing.Instagram.cs
+++ iOS/Sharing.Instagram.cs
@@ -1,13 +1,11 @@
-﻿using System;
-
-namespace Zebble.Device
+﻿namespace Zebble.Device
 {
     public partial class Sharing
     {
         public partial class Instagram
         {
             public static void ShareFile(string url, string title)
-                => throw new NotImplementedException();
+                => ShareFileToPackage("com.instagram.android", url, title);
         }
     }
 }

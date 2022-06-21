4d0d7006de98c2778e60da7e9a64d367ffc4a3df
Mohsen Seifi <mohsens2@outlook.com>
2022-06-21 18:22:21 +0430

    Added UWP image sharing implemenation Andded an iOS implementation

--- UWP/Sharing.Instagram.cs
+++ UWP/Sharing.Instagram.cs
@@ -2,12 +2,41 @@
 
 namespace Zebble.Device
 {
+    using Olive;
+    using System.Collections.Generic;
+    using Windows.ApplicationModel.DataTransfer;
+    using Windows.Foundation;
+    using Windows.Storage;
+    using Windows.Storage.Streams;
+
     public partial class Sharing
     {
         public partial class Instagram
         {
             public static void ShareFile(string url, string title)
-                => throw new NotImplementedException();
+            { 
+                Thread.UI.Run(() =>
+                {
+                    var manager = DataTransferManager.GetForCurrentView();
+                    manager.DataRequested +=
+                        new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>((s, e) => ShareImage(s, e, url, title));
+                    DataTransferManager.ShowShareUI();
+                });
+                
+            }
+
+            private static async void ShareImage(DataTransferManager sender, DataRequestedEventArgs eventArgs, string url, string title)
+            {
+                var tempExportFile = await StorageFile.GetFileFromPathAsync(url);
+                var imageItems = new List<IStorageItem> { tempExportFile };
+
+                var imageReference = RandomAccessStreamReference.CreateFromFile(tempExportFile);
+                eventArgs.Request.Data.SetStorageItems(imageItems);
+                eventArgs.Request.Data.SetBitmap(imageReference);
+                eventArgs.Request.Data.Properties.Thumbnail = imageReference;
+                eventArgs.Request.Data.Properties.Title = title;
+
+            }
         }
     }
 }
--- iOS/Sharing.Instagram.cs
+++ iOS/Sharing.Instagram.cs
@@ -1,11 +1,34 @@
-﻿namespace Zebble.Device
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace Zebble.Device
 {
+    
+
     public partial class Sharing
     {
         public partial class Instagram
         {
-            public static void ShareFile(string url, string title)
-                => ShareFileToPackage("com.instagram.android", url, title);
+            public static bool ShareFile(string path, string title)
+            {
+
+                return Thread.UI.Run<bool>(() =>
+                {
+                    string uti = "com.instagram.exclusivegram";
+                    var controller = new UIDocumentInteractionController
+                    {
+                        Url = new NSUrl(path,false),
+                        Uti = uti
+                    };
+                    return controller.PresentOptionsMenu(UIScreen.MainScreen.Bounds, UIApplication.SharedApplication.Delegate.GetWindow(), true);
+                });
+
+                
+            }
         }
     }
 }
